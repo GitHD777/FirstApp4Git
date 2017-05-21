3752066399ce5eb92e427385e67b4014ee397b8f
GitHD777 <hasith.disanayake@gmail.com>
2017-05-21 16:22:14 -0500

    Initial project check in

---
+++ app/src/main/java/com/hdapps/firstapp4git/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hdapps.firstapp4git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
